08d6c6ab6709785b62ba2592ab917dbbd251bca3
melon <matteobecatti@protonmail.com>
2025-10-20 15:45:06 +0200

    Wrote down a simple, barebone main.py

---
+++ src/main.py
@@ -0,0 +1,33 @@
+def main():
+    flag = True
+    while flag:
+        print("\t- 1) Load a basic text file")
+        print("\t- 2) Display basic statistics")
+        print("\t- 3) Show word frequency analysis")
+        print("\t- 4) Display sentence analysis")
+        print("\t- 5) Display character analysis")
+        print("\t- 6) Export results")
+        print("\t- 0) Exit program")
+
+        choice = input("\tInsert your choice:")
+
+        match choice:
+            case "1":
+                print("Choice one!")
+            case "2":
+                print("Choice two!")
+            case "3":
+                print("Choice three!")
+            case "4":
+                print("Choice four!")
+            case "5":
+                print("Choice five!")
+            case "6":
+                print("Choice six!")
+            case "0":
+                print("Exiting the program...")
+                flag = False
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+main()
