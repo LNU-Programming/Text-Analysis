7ed130123cbc9e9de8c62e0e964b5e5fef1aa7b5
philipzara <filipzara@gmail.com>
2025-10-25 14:08:43 +0200

    Changes by melon

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
+import file_loading
+
 def main():
     flag = True
+    statistics = {"filename": ""} # dictionary containing statistics for the whole book, after we selected it
     while flag:
         print("\t 1) Load a basic text file")
         print("\t 2) Display basic statistics")
@@ -8,12 +11,13 @@ def main():
         print("\t 5) Display character analysis")
         print("\t 6) Export results")
         print("\t 0) Exit program")
+        print(f"\t\n The file you selected is: {statistics["filename"]}")
 
         choice = input("\tInsert your choice: ")
 
         match choice:
             case "1":
-                print("Choice one!")
+                statistics["filename"] = file_loading.file_loading()
             case "2":
                 print("Choice two!")
             case "3":
