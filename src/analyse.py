c50a9266cfa436c859ea6eb319a27fdd90c6a5eb
philipzara <filipzara@gmail.com>
2025-10-25 17:33:38 +0200

    Basic analysis done, menu updated with words analysis backbone

--- src/analyse.py
+++ src/analyse.py
@@ -4,26 +4,32 @@ def analyse_file(path: str, filename: str) -> dict:
                   "total_words": 0,
                   "total_characters_with_spaces": 0,
                   "total_characters_without_spaces": 0,
-                  "avg_words_per_line": 0,
-                  "avg_char_per_word": 0}
+                  "avg_words_per_line": 0.0,
+                  "avg_char_per_word": 0.0}
 
     try:
         with open(f"{path}{filename}", "r") as file:
             temp_word = ''
             for line in file:
                 for char in line:
+                    statistics['total_characters_with_spaces'] += 1
                     if char.isalpha():
                         temp_word += char.lower()
                     else:
                         if temp_word != '':
                             statistics['total_words'] += 1
+                            statistics['total_characters_without_spaces'] += len(temp_word)
                             if temp_word in statistics:
                                 statistics[temp_word] += 1
                             else:
                                 statistics[temp_word] = 1
-
                         temp_word = ''
+
                 statistics['total_lines'] += 1
+
+            statistics['avg_words_per_line'] = statistics['total_words'] / statistics['total_lines']
+            statistics['avg_char_per_word'] = statistics['total_characters_without_spaces'] / statistics['total_words']
+
     except FileNotFoundError:
         print("File not found.")
 
--- src/main.py
+++ src/main.py
@@ -6,8 +6,29 @@ def display_basic_statistics(statistics: dict) -> None:
     print(f'Words: {statistics["total_words"]}')
     print(f'Characters (with spaces): {statistics["total_characters_with_spaces"]}')
     print(f'Characters (without spaces): {statistics["total_characters_without_spaces"]}')
-    print(f'Average words per line: {statistics["avg_words_per_line"]}')
-    print(f'Average word length: {statistics["avg_char_per_word"]}')
+    print(f'Average words per line: {statistics["avg_words_per_line"]:.2f}')
+    print(f'Average word length: {statistics["avg_char_per_word"]:.2f}')
+
+    print("\nGenerating basic statistics visualization...")
+    # TODO: generate Matplotlib visualization
+    print("Press ENTER to continue...")
+    return None
+
+def display_word_analysis(statistics) -> None:
+    print(f'--- Word Analysis for "{statistics["filename"]}" ---')
+    print("Top 10 most common words:")
+    for i in range(1, 11):
+        print(f"{i}. aaaa\t\t0 times (0 %)")
+
+    print("\nWord length statistics:")
+    print(f"\tShortest word: 0 characters")
+    print(f"\tLongest word: 0 characters")
+    print(f"\tAverage word length: 0 characters")
+    print(f"Words appearing only once: 0")
+
+    print("\nGenerating word analysis visualization...")
+    # TODO: generate Matplotlib visualization
+    print("Press ENTER to continue...")
     return None
 
 def main():
@@ -35,7 +56,7 @@ def main():
             case "2":
                 display_basic_statistics(statistics)
             case "3":
-                print("Choice three!")
+                display_word_analysis(statistics)
             case "4":
                 print("Choice four!")
             case "5":
