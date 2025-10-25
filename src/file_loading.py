7f291d41181c645ccb30a447c45347aed783a280
philipzara <filipzara@gmail.com>
2025-10-25 14:30:52 +0200

    Minor changes

--- src/file_loading.py
+++ src/file_loading.py
@@ -1,23 +1,20 @@
 import os
 
-
-
 def file_loading():
     print("\n--- File Selection ---\nAvailable text files:")
     # TODO: If no files ar present, return nothing
+    # TODO: Add try catch when reading a file, since it might raise an exception and we don't want that
     for index, x in enumerate(os.listdir('../data/')):
         if x.endswith(".txt"):
-            # Prints only text file present in My Folder
             print(f'{index+1}) {x}')
 
-    while True:
+    while True: # TODO: not allowed, rewrite it without while True
         try:
             choice = int(input("Enter a number from the list above. "))
             selected_file = os.listdir('../data/')
             if 0 < choice < len(selected_file):
-                # TODO: call file analysis
+                # TODO: call file analysis from here, we are just returning the book right now
                 return selected_file[choice - 1]
-            else:
-                print(f"Please enter a number between 1 and {len(selected_file)}")
+            print(f"Please enter a number between 1 and {len(selected_file)}")
         except ValueError:
             print("Invalid input!")
