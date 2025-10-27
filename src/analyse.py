babbe741535137dd5f6cf60f84e472037ff34133
philipzara <filipzara@gmail.com>
2025-10-27 12:31:32 +0100

    longest and shortest sentence done

--- src/analyse.py
+++ src/analyse.py
@@ -41,11 +41,11 @@ def initialize_statistics(filename: str) -> dict[str, any]:
         "avg_word_length": 0.0,
         "word_length_distribution": [],
         "unique_word_count": 0,
-        "words_appearing_only_once": 0, # TODO
+        "words_appearing_only_once": 0,
         # ==== Sentence analysis ====
         "average_words_per_sentence": 0.0, # TODO
         "longest_sentence": "", # TODO
-        "shortest_sentence": "", # TODO
+        "shortest_sentence": "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", # TODO
         "sentence_length_distribution": [], # TODO
         # ==== Character analysis ====
         "total_letters": 0, # TODO
@@ -84,6 +84,8 @@ def process_character(char: str, statistics: dict, analysis_data: dict) -> None:
     # Check if we are at the end of a sentence
     if char in SENTENCE_ENDERS:
         analysis_data["sentence_lengths"].append(len(analysis_data["current_sentence"]))
+        statistics['longest_sentence'] = analysis_data['current_sentence'] if len(analysis_data['current_sentence']) > len(statistics['longest_sentence']) else statistics['longest_sentence']
+        statistics['shortest_sentence'] = analysis_data['current_sentence'] if len(analysis_data['current_sentence']) < len(statistics['shortest_sentence']) else statistics['shortest_sentence']
         analysis_data["current_sentence"] = ""
         statistics["total_sentences"] += 1
 
