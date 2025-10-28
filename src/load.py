b4a1ecb0684505c0afed0a5de807a86f05e3c856
melon <matteobecatti@protonmail.com>
2025-10-28 11:00:37 +0100

    Scribbled an implementation of filename typing during choice, check if works before merging

--- src/load.py
+++ src/load.py
@@ -2,15 +2,16 @@ import os
 import analyse
 
 # Color codes
-RED = '\033[91m'
-GREEN = '\033[92m'
-YELLOW = '\033[93m'
-BLUE = '\033[94m'
-MAGENTA = '\033[95m'
-CYAN = '\033[96m'
-WHITE = '\033[97m'
-RESET = '\033[0m'  # Reset to default color
-BOLD = '\033[1m'
+RED = "\033[91m"
+GREEN = "\033[92m"
+YELLOW = "\033[93m"
+BLUE = "\033[94m"
+MAGENTA = "\033[95m"
+CYAN = "\033[96m"
+WHITE = "\033[97m"
+RESET = "\033[0m"  # Reset to default color
+BOLD = "\033[1m"
+
 
 def load_file() -> dict[str, any]:
     path = "../data/"
@@ -24,14 +25,22 @@ def load_file() -> dict[str, any]:
         return {"filename": ""}
 
     try:
-        choice = int(input("Enter a number from the list above. "))
+        choice = input(
+            "Enter a number from the list above, or write the name of the file."
+        )
+
         # TODO: add possibility to type filename aswell
+
         selected_files = os.listdir("../data/")
         if 0 < choice <= len(selected_files):
             print(f"\nAnalyzing {selected_files[choice - 1]}...")
             return analyse.analyse_file(path, selected_files[choice - 1])
+        elif choice in selected_files:
+            print(f"\nAnalyzing {choice}...")
+            return analyse.analyse_file(path, choice)
         print(f"{RED}Please enter a number between 1 and {len(selected_files)}{RESET}")
         return load_file()
+
     except ValueError:
         print(f"{RED}Invalid input!{RESET}")
         return load_file()
