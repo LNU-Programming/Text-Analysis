869716e56ef48c324fd08d2ec682287fc0ba19f6
melon <matteobecatti@protonmail.com>
2025-10-29 09:37:31 +0100

    Now the files are automatically created/rewritten, error checking if problems during IO

--- src/export.py
+++ src/export.py
@@ -6,6 +6,11 @@ def export_to_txt(statistics: dict):
     print(f'Results exported to {PATH}{export_name}')
     print('Results saved in simple text format - easy to read and understand!')
 
-
-
+    try:
+        with open(PATH + export_name, 'w') as file:
+            file.write('Ciao mi chiamo fagiolo\n')
+            file.write('Ciaoissimooo\n')
+            file.write('Cia cia')
+    except IOError:
+        print('There was an error writing to file!')
     return None
