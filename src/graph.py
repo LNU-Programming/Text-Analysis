0fcd9987c63a70b40efd7edbfe99c5caff8a8ad7
melon <matteobecatti@protonmail.com>
2025-10-29 16:04:17 +0100

    Added titles to all the graphs

--- src/graph.py
+++ src/graph.py
@@ -7,6 +7,7 @@ def character_type_distribution(stats):
               stats['total_spaces'], stats['total_punctuation']]
 
     plt.pie(values, labels=labels)
+    plt.title('Character Type Distribution')
     plt.show()
 
 
@@ -16,6 +17,7 @@ def text_composition(stats):
               stats['total_sentences'], stats['total_words']]
 
     plt.bar(categories, values)
+    plt.title('Text Composition')
     plt.show()
 
 
@@ -24,6 +26,7 @@ def most_common_words_graph(stats):
     counts = list(stats['ten_most_common_words'].values())
 
     plt.bar(words, counts)
+    plt.title('10 Most Common Words')
     plt.show()
 
 
@@ -32,6 +35,7 @@ def word_length_distribution_graph(stats):
     counts = stats['word_length_distribution']
 
     plt.bar(lengths, counts)
+    plt.title('Word Length Distribution')
     plt.show()
 
 
@@ -40,6 +44,7 @@ def sentence_length_distribution_graph(stats):
     counts = stats['sentence_length_distribution']
 
     plt.bar(lengths, counts)
+    plt.title('Sentence Length Distribution')
     plt.show()
 
 
@@ -57,11 +62,14 @@ def most_common_sentence_length(stats):
     counts = [pair[1] for pair in top_8]
 
     plt.bar(lengths, counts)
+    plt.title('8 Most Common Sentence Lengths')
     plt.show()
+
 
 def ten_most_common_letters_graph(stats):
     letters = list(stats['ten_most_common_letters'].keys())
     counts = list(stats['ten_most_common_letters'].values())
 
     plt.bar(letters, counts)
+    plt.title('10 Most Common Letters')
     plt.show()
